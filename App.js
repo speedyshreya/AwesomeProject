5c7e48b9d5209be01ce347ec51fd163b343f6cf7
speedyshreya <shreya251199@gmail.com>
2022-08-25 12:46:55 +0530

    navigator

--- App.js
+++ App.js
@@ -1,47 +1,52 @@
-import React from "react";
-import { Text, View, Modal, Button, StyleSheet, TouchableOpacity, AppRegistry} from "react-native";
-import { Component } from "react";
-import { SafeAreaView } from "react-native";
-//import {Constants} from 'expo'; 
-import type { Node } from "react";
-
-
-export default class MyComponent extends Component {
-  constructor(props){
-    super(props);
-
-    this.increaseByOne = this.increaseByOne.bind(this);
-    this.state = {
-      myInteger:0
-    }
-  }
-
-  increaseByOne(){
-    this.setState(previousState => {
-      return {
-        myInteger: previousState.myInteger + 1
-      }
-    });
-  }
-
-  render(){
-    return(
-      <View style = {styles.container}>
-        <Text>Integer: {this.state.myInteger}</Text>
-        <Button label = "Press to increase" onPress={this.increaseByOne} />
-      </View>
-    )
-  }
+import React, { Component } from 'react';
+import { Text, Navigator, TouchableHighlight } from 'react-native';
+export default class NavAllDay extends Component { render() {
+return ( <Navigator
+        initialRoute={{ title: 'Awesome Scene', index: 0 }}
+        renderScene={(route, navigator) =>
+          <Text>Hello {route.title}!</Text>
+        }
+        style={{padding: 100}}
+      />
+); }
 }
 
-const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        alignItems: 'center',
-       justifyContent: 'center',
-        //paddingTop: Constants.statusBarHeight,
-        backgroundColor: '#ecf0f1',
-      }
-    });
+// export default class MyComponent extends Component {
+//   constructor(props){
+//     super(props);
+
+//     //this.increaseByOne = this.increaseByOne.bind(this);
+//     this.state = {
+//       myInteger:0
+//     }
+//   }
+
+//   increaseByOne(){
+//     this.setState(previousState => {
+//       return {
+//         myInteger: previousState.myInteger + 1
+//       }
+//     });
+//   }
+
+//   render(){
+//     return(
+//       <View style = {styles.container}>
+//         <Text>Integer: {this.state.myInteger}</Text>
+//         <Button title = "Press to increase" onPress={this.increaseByOne.bind(this)} />
+//       </View>
+//     )
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//       container: {
+//         flex: 1,
+//         alignItems: 'center',
+//        justifyContent: 'center',
+//         //paddingTop: Constants.statusBarHeight,
+//         backgroundColor: '#ecf0f1',
+//       }
+//     });
   
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+//import NavAllDay from './App';
 import {name as appName} from './app.json';
-
+import NavAllDay from './App'
 AppRegistry.registerComponent(appName, () => App);
